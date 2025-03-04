04f4cf817ed42cb4a50565cd787a8fd5368f41d3
taejung3852 <taejung3852@gmail.com>
2025-03-04 21:35:19 +0900

    학습: Vue.js 데이터 바인딩 실습
    Vue.js의 데이터 바인딩 개념을 학습하고 실습을 진행함.
    - 배열을 활용하여 데이터를 저장하고 인덱스별로 바인딩.
    - template에서 바인딩된 값을 출력하여 확인.
    
    오늘의 학습 내용 정리: 2025년 03월 04일

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p> 50 만원</p>
+  </div>
+  <div>
+    <h4>{{products[1]}}</h4>
+    <p>70 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>90 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  // 데이터 보관소. return 안에 데이터를 입력하면된다. 변수같은거 return 안에 다 넣는다고 생각하면된다. object 자료(키:밸류)로 저장.
+  // (중요)이 문법을 언제 왜 쓰는지 배워야한다.
+  data(){
+    return{
+      products : ['신부동원룸', '안서동원룸', '두정동원룸']
+    }
+  },
   components: {
-    HelloWorld
+
   }
 }
 </script>
