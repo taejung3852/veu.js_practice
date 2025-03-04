047676a6f421b937a8c4fd9309ea3e6fe224af32
taejung3852 <taejung3852@gmail.com>
2025-03-04 23:04:44 +0900

    학습: Vue.js 이벤트 핸들러 및 methods 활용
    Vue.js의 이벤트 핸들러를 기존 JavaScript 문법과 비교하여 학습.
    
    이벤트 발생 시 실행할 코드가 길어질 경우를 대비해 methods{} 내부에 함수 정의 및 적용.
    
    오늘의 학습 내용 정리: 실습을 통해 Vue.js 이벤트 처리 방식 이해도 향상.
    
    2025년 03월 04일

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,13 @@
   <!-- class 속성으로 해당 태그의 이름을 붙여줄 수 있다. -->
   <!-- 이 이름의 역할은 css에서 불러와서 꾸며줄 수 있다. -->
   <div class="menu">
-    <a v-for="작명 in menus" :key="작명">{{작명}}</a>
+    <a v-for="menu in menus" :key="menu">{{menu}}</a>
   </div>
 
-  <div v-for="(product, i) in products" :key="product">
-    <h4>{{ product }}</h4>
+  <div v-for="(_, i) in products" :key="i"> <!--:key 뒤에는 고유한 값을 넣어줘야하기 때문에 i(index)를 넣어주는 것이 좋다.-->
+    <h4>{{ products[i] }}</h4>
     <p>{{prices[i]}} 만원</p>
+    <button @click=increase(i)>허위매물신고</button> <span>신고수 : {{ reports[i] }}</span>
   </div>
   
 </template>
@@ -21,9 +22,15 @@ export default {
   // (중요)이 문법을 언제 왜 쓰는지 배워야한다.
   data(){
     return{
+      reports : [0, 0, 0],
       products : ['신부동원룸', '안서동원룸', '두정동원룸'],
       menus : ['Home', 'Shop', 'About'],
       prices : [50, 70, 90]
+    }
+  },
+  methods : {
+    increase(i){
+      this.reports[i]++;
     }
   },
   components: {
